4a810bd757f0b9cba478d9c7dda226b5c4b07421
Alexsander Tsova <tsova@bk.ru>
2017-02-09 00:25:41 +0200

    Add 3 level

---
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0217/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task02.task0217;
+
+/* 
+Минимум четырех чисел
+*/
+public class Solution {
+//    public static int min(int a, int b, int c, int d) {
+//        //напишите тут ваш код
+//
+//    }
+//
+//    public static int min(int a, int b) {
+//        //напишите тут ваш код
+//
+//    }
+//
+//    public static void main(String[] args) throws Exception {
+//        System.out.println(min(-20, -10));
+//        System.out.println(min(-20, -10, -30, -40));
+//        System.out.println(min(-20, -10, -30, 40));
+//    }
+}
