f62a18e5f20347200d1d2fcbd3a4b36489888f20
Alex Tsova <a.tsova@loopme.com>
2017-02-13 23:22:03 +0200

    try to do on macbook

--- 1.JavaSyntax/src/com/javarush/task/task05/task0507/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0507/Solution.java
@@ -4,9 +4,34 @@ package com.javarush.task.task05.task0507;
 Среднее арифметическое
 */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> list = new ArrayList<>();
+        String temp = "";
+        int cisl = 0;
+        int znam = 0;
+
+        while (true) {
+            temp = reader.readLine();
+            if (temp.equals("-1")) {
+                znam = list.size();
+
+                for (Integer number : list){
+                    cisl += number;
+                }
+                System.out.println((double) cisl/znam);
+                return;
+            } else {
+                list.add(Integer.parseInt(temp));
+            }
+        }
     }
 }
 
--- 1.JavaSyntax/src/com/javarush/task/task05/task0515/Person.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0515/Person.java
@@ -11,20 +11,28 @@ public class Person {
     int weight;
     double size;
 
-    public void initialize(String name) {
-        this.name = name;
-    }
+//    public void initialize(String name) {
+//        this.name = name;
+//    }
+//
+//    public void initialize(String name, char sex) {
+//        this.name = name;
+//        this.sex = sex;
+//    }
 
-    public void initialize(String name, char sex) {
+    public void initialize(String name, char sex, int money, int weight, double size) {
         this.name = name;
         this.sex = sex;
-    }
-
-    public void initialize(String name, int money, char sex) {
-        this.name = name;
         this.money = money;
-        this.sex = sex;
+        this.weight = weight;
+        this.size = size;
     }
+
+//    public void initialize(String name, int money, char sex,) {
+//        this.name = name;
+//        this.money = money;
+//        this.sex = sex;
+//    }
 
     public static void main(String[] args) {
 
