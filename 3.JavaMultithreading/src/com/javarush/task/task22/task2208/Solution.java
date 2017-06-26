f34c72ffaf2a59f0b7ccd1ae800e61ec01d884e3
Alex Tsova <a.tsova@loopme.com>
2017-06-26 12:01:32 +0300

    Task from 22 level

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2208/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task22.task2208;
+
+import java.util.Map;
+
+/* 
+Формируем WHERE
+*/
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public static String getQuery(Map<String, String> params) {
+        return null;
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2211/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task22.task2211;
+
+import java.io.IOException;
+
+/* 
+Смена кодировки
+*/
+public class Solution {
+    static String win1251TestString = "РќР°СЂСѓС€РµРЅРёРµ РєРѕРґРёСЂРѕРІРєРё РєРѕРЅСЃРѕР»Рё?"; //only for your testing
+
+    public static void main(String[] args) throws IOException {
+    }
+}
