5301ffc7faecc99ca7c8377b88ee00707b40e943
Alex Tsova <a.tsova@loopme.com>
2017-08-11 15:49:20 +0300

    done previous task

--- 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
@@ -1,7 +1,12 @@
 package com.javarush.task.task19.task1916;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /* 
 Отслеживаем изменения
@@ -10,7 +15,33 @@ import java.util.List;
 public class Solution {
     public static List<LineItem> lines = new ArrayList<LineItem>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             FileReader fileReader1 = new FileReader(reader.readLine());
+             FileReader fileReader2 = new FileReader(reader.readLine())) {
+
+            List<String> original = new BufferedReader(fileReader1).lines().collect(Collectors.toList());
+            List<String> modified = new BufferedReader(fileReader2).lines().collect(Collectors.toList());
+
+            while (original.size() != 0 & modified.size() != 0) {
+                if (original.get(0).equals(modified.get(0))) {
+                    lines.add(new LineItem(Type.SAME, original.remove(0)));
+                    modified.remove(0);
+                } else if (modified.size() != 1 && original.get(0).equals(modified.get(1))) {
+                    lines.add(new LineItem(Type.ADDED, modified.remove(0)));
+                } else if (original.size() != 1 && original.get(1).equals(modified.get(0))) {
+                    lines.add(new LineItem(Type.REMOVED, original.remove(0)));
+                }
+            }
+
+            if (original.size() != 0) {
+                lines.add(new LineItem(Type.REMOVED, original.remove(0)));
+            } else if (modified.size() != 0) {
+                lines.add(new LineItem(Type.ADDED, modified.remove(0)));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
--- 2.JavaCore/src/com/javarush/task/task19/task1917/FileConsoleWriter.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1917/FileConsoleWriter.java
@@ -4,9 +4,59 @@ package com.javarush.task.task19.task1917;
 Свой FileWriter
 */
 
-public class FileConsoleWriter {
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileConsoleWriter extends FileWriter {
+    public FileConsoleWriter(String fileName) throws IOException {
+        super(fileName);
+    }
+
+    public FileConsoleWriter(String fileName, boolean append) throws IOException {
+        super(fileName, append);
+    }
+
+    public FileConsoleWriter(File file) throws IOException {
+        super(file);
+    }
+
+    public FileConsoleWriter(File file, boolean append) throws IOException {
+        super(file, append);
+    }
+
+    public FileConsoleWriter(FileDescriptor fd) {
+        super(fd);
+    }
+
     public static void main(String[] args) {
 
     }
 
+    @Override
+    public void write(int c) throws IOException {
+        super.write(c);
+    }
+
+    @Override
+    public void write(char[] cbuf) throws IOException {
+        super.write(cbuf);
+    }
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        super.write(cbuf, off, len);
+    }
+
+    @Override
+    public void write(String str) throws IOException {
+        super.write(str);
+    }
+
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        super.write(str, off, len);
+    }
 }
