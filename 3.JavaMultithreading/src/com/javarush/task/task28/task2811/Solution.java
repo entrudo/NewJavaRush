5e87ee096edea847f987fa79db5a96d50183f4f9
Alex Tsova <a.tsova@loopme.com>
2017-08-16 14:07:46 +0300

    Task from 27 and 28 level

---
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2811/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task28.task2811;
+
+/* 
+ReentrantReadWriteLock
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        ReadWriteMap<Integer, String> linkedSafeMap = new ReadWriteMap<>(new LinkedHashMap<>());
+    }
+}
