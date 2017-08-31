59d9cc80f3c91f86f4319504c334ee63c246b5b9
Alex Tsova <a.tsova@loopme.com>
2017-08-31 16:37:01 +0300

    git task MVC #5

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.model.service.UserService;
+import com.javarush.task.task36.task3608.model.service.UserServiceImpl;
+
+public class MainModel implements Model {
+    private UserService userService = new UserServiceImpl();
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        modelData.setUsers(userService.getUsersBetweenLevels(1, 100));
+
+    }
+}
