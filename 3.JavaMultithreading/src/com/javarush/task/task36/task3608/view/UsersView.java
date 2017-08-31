e0dbf695228c8900f86ea79e8ffbc304a30f05ea
Alex Tsova <a.tsova@loopme.com>
2017-08-31 16:26:04 +0300

    git task MVC #4

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/UsersView.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task36.task3608.view;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.controller.Controller;
+import com.javarush.task.task36.task3608.model.ModelData;
+
+public class UsersView implements View {
+    private Controller controller;
+
+
+    @Override
+    public void refresh(ModelData modelData) {
+        System.out.println("All users:");
+        for (User user : modelData.getUsers()) {
+            System.out.println("\t" + user.toString());
+        }
+        System.out.println("===================================================");
+    }
+
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public void fireEventShowAllUsers() {
+        controller.onShowAllUsers();
+    }
+}
