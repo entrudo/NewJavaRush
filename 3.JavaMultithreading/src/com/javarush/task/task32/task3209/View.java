ced70166d505cde4ffaac917cfd6fa0022597d08
Alex Tsova <a.tsova@loopme.com>
2017-09-01 10:46:59 +0300

    html editor big 5

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task32.task3209;
 
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -24,7 +26,23 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void init() {
+        initGui();
+        addWindowListener(new FrameListener(this));
+        setVisible(true);
+    }
 
+    public void initMenuBar() {
+
+    }
+
+    public void initEditor() {
+
+    }
+
+    public void initGui() {
+        initMenuBar();
+        initEditor();
+        pack();
     }
 
     public void exit() {
