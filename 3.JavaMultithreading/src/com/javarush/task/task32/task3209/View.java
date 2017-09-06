3b1143cd65aff105865d08622d80f39d461e581f
Alex Tsova <a.tsova@loopme.com>
2017-09-06 11:36:39 +0300

    html editor big 15

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -123,4 +123,18 @@ public class View extends JFrame implements ActionListener {
     public boolean isHtmlTabSelected() {
         return tabbedPane.getSelectedIndex() == 0;
     }
+
+    public void selectHtmlTab() {
+        tabbedPane.setSelectedIndex(0);
+        resetUndo();
+    }
+
+    public void update() {
+        htmlTextPane.setDocument(controller.getDocument());
+    }
+
+    public void showAbout() {
+        JOptionPane.showMessageDialog(getContentPane(), "HTML editor version 1.0",
+                "About program", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
