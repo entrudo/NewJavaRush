a0313d8e8af3642167f28919e18ae59c61d59060
Alex Tsova <a.tsova@loopme.com>
2017-09-06 11:28:37 +0300

    html editor big 14

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -119,4 +119,8 @@ public class View extends JFrame implements ActionListener {
     public void resetUndo() {
         undoManager.discardAllEdits();
     }
+
+    public boolean isHtmlTabSelected() {
+        return tabbedPane.getSelectedIndex() == 0;
+    }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/TextEditMenuListener.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/TextEditMenuListener.java
@@ -2,16 +2,24 @@ package com.javarush.task.task32.task3209.listeners;
 
 import com.javarush.task.task32.task3209.View;
 
+import javax.swing.*;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
+import java.awt.*;
 
 public class TextEditMenuListener implements MenuListener {
+    private View view;
 
     public TextEditMenuListener(View view) {
+        this.view = view;
     }
 
     @Override
     public void menuSelected(MenuEvent e) {
+        JMenu jMenu = (JMenu) e.getSource();
+        for (Component component : jMenu.getMenuComponents()) {
+            component.setEnabled(view.isHtmlTabSelected());
+        }
 
     }
 
