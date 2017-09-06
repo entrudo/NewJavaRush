25189be6b31b58c0a5e0f615e1368cb99bf7965a
Alex Tsova <a.tsova@loopme.com>
2017-09-06 12:28:53 +0300

    html editor big 22

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -74,6 +74,7 @@ public class Controller {
     }
 
     public void openDocument() {
+
     }
 
     public void saveDocument() {
---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/HTMLFileFilter.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task32.task3209;
+
+import javax.swing.filechooser.FileFilter;
+import java.io.File;
+
+public class HTMLFileFilter extends FileFilter {
+    @Override
+    public boolean accept(File f) {
+        if (f.isDirectory()) {
+            return true;
+        } else {
+            return f.getName().toLowerCase().contains(".html") || f.getName().toLowerCase()
+                    .contains(".htm");
+        }
+    }
+
+    @Override
+    public String getDescription() {
+        return "HTML и HTM файлы";
+    }
+}
