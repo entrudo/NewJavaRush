a01faae188c9369406821ea7081c4ad1697ff18a
Alex Tsova <a.tsova@loopme.com>
2017-09-12 14:24:02 +0300

    big task from 27 level big 2

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Dish.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task27.task2712.kitchen;
+
+import java.util.Arrays;
+
+public enum Dish {
+    Fish, Steak, Soup, Juice, Water;
+
+    public static String allDishesToString() {
+        return Arrays.toString(Dish.values());
+    }
+}
