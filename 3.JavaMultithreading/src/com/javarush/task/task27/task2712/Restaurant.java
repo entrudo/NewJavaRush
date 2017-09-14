e1dea2b988d5a271c908e2867dcc6f19acbf1230
Alex Tsova <a.tsova@loopme.com>
2017-09-14 16:28:55 +0300

    big task from 27 level big 5

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,13 +1,16 @@
 package com.javarush.task.task27.task2712;
 
 import com.javarush.task.task27.task2712.kitchen.Cook;
+import com.javarush.task.task27.task2712.kitchen.Waiter;
 
 import java.io.IOException;
 
 public class Restaurant {
     public static void main(String[] args) throws IOException {
         Cook cook = new Cook("Vasya");
+        Waiter waiter = new Waiter();
         Tablet tablet = new Tablet(5);
+        cook.addObserver(waiter);
         tablet.addObserver(cook);
         tablet.createOrder();
 
