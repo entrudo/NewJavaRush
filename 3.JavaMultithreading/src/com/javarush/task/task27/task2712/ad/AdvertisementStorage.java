6288053bb68de92f28b5a5d60e35b5dc65d4d46f
Alex Tsova <a.tsova@loopme.com>
2017-09-18 17:23:57 +0300

    big task from 27 level big 8

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -1,9 +1,18 @@
 package com.javarush.task.task27.task2712.ad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdvertisementStorage {
     private static AdvertisementStorage advertisementStorage;
+    private final List<Advertisement> videos = new ArrayList<Advertisement>();
+
 
     private AdvertisementStorage() {
+        Object someContent = new Object();
+        new Advertisement(someContent, "First Video", 5000, 100, 3 * 60); // 3 min
+        new Advertisement(someContent, "Second Video", 100, 10, 15 * 60); //15 min
+        new Advertisement(someContent, "Third Video", 400, 2, 10 * 60); //10 min
     }
 
     public static AdvertisementStorage getInstance() {
@@ -12,4 +21,14 @@ public class AdvertisementStorage {
         }
         return advertisementStorage;
     }
+
+    public List<Advertisement> list() {
+        return videos;
+    }
+
+    public void add(Advertisement advertisement) {
+        videos.add(advertisement);
+    }
+
+
 }
