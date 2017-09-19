73e9557888ac1eb29b8710d7b62cac00af2390b3
Alex Tsova <a.tsova@loopme.com>
2017-09-19 16:27:08 +0300

    big task from 27 level big 14

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -1,9 +1,16 @@
 package com.javarush.task.task27.task2712.statistic;
 
 import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+import com.javarush.task.task27.task2712.statistic.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StatisticManager {
     private static StatisticManager instance;
+    private StatisticStorage statisticStorage = new StatisticStorage();
 
     private StatisticManager() {
     }
@@ -17,5 +24,15 @@ public class StatisticManager {
 
     public void register(EventDataRow data) {
 
+    }
+
+    private static class StatisticStorage {
+        private Map<EventType, List<EventDataRow>> storage = new HashMap<>();
+
+        public StatisticStorage() {
+            for (EventType eventType : EventType.values()) {
+                storage.put(eventType, new ArrayList<EventDataRow>());
+            }
+        }
     }
 }
