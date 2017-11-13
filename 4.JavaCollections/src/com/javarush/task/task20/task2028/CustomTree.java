2e69425ad1ae38d02bf3dcddeabdb66acc07cc08
Alex Tsova <a.tsova@loopme.com>
2017-11-13 11:44:04 +0200

    big 3 from 2028 task

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -2,6 +2,7 @@ package com.javarush.task.task20.task2028;
 
 import java.io.Serializable;
 import java.util.AbstractList;
+import java.util.Collection;
 import java.util.List;
 
 /* 
@@ -18,13 +19,43 @@ public class CustomTree extends AbstractList implements Cloneable, Serializable 
         //System.out.println("Expected null, actual is " + ((CustomTree) list).getParent("11"));
     }
 
-    @Override
-    public Object get(int index) {
-        return null;
+    public String get(int index) {
+        throw new UnsupportedOperationException();
     }
+
+    public String set(int index, String element) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void add(int index, String element) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String remove(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    public List<String> subList(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
+    }
+
+    protected void removeRange(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int index, Collection c) {
+        throw new UnsupportedOperationException();
+    }
+
+//    @Override
+//    public Object get(int index) {
+//        return null;
+//    }
 
     @Override
     public int size() {
         return 0;
     }
+
 }
