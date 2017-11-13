c568d96301ea1a5f3d90a606e5e42e4a92e49f5e
Alex Tsova <a.tsova@loopme.com>
2017-11-13 11:36:38 +0200

    big 1 from 2028 task

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -1,18 +1,30 @@
-//package com.javarush.task.task20.task2028;
-//
-//import java.util.List;
-//
-///*
-//Построй дерево(1)
-//*/
-//public class CustomTree {
-//    public static void main(String[] args) {
-//        List<String> list = new CustomTree();
-//        for (int i = 1; i < 16; i++) {
-//            list.add(String.valueOf(i));
-//        }
-//        //System.out.println("Expected 3, actual is " + ((CustomTree) list).getParent("8"));
-//        list.remove("5");
-//        //System.out.println("Expected null, actual is " + ((CustomTree) list).getParent("11"));
-//    }
-//}
+package com.javarush.task.task20.task2028;
+
+import java.io.Serializable;
+import java.util.AbstractList;
+import java.util.List;
+
+/* 
+Построй дерево(1)
+*/
+public class CustomTree extends AbstractList implements Cloneable, Serializable {
+    public static void main(String[] args) {
+        List<String> list = new CustomTree();
+        for (int i = 1; i < 16; i++) {
+            list.add(String.valueOf(i));
+        }
+        //System.out.println("Expected 3, actual is " + ((CustomTree) list).getParent("8"));
+        list.remove("5");
+        //System.out.println("Expected null, actual is " + ((CustomTree) list).getParent("11"));
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
--- 4.JavaCollections/src/com/javarush/task/task31/task3106/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3106/Solution.java
@@ -1,22 +1,48 @@
 package com.javarush.task.task31.task3106;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.SequenceInputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.zip.ZipInputStream;
 
 /*
 Разархивируем файл
 */
 public class Solution {
-    public static void main(String[] args) {
-        String resultFile = args[0];
+    public static void main(String[] args) throws IOException {
+        String resultFileName = args[0];
+        int filePartCount = args.length - 1;
+        String[] fileNamePart = new String[filePartCount];
+        for (int i = 0; i < filePartCount; i++) {
+            fileNamePart[i] = args[i + 1];
+        }
+        Arrays.sort(fileNamePart);
 
-        String[] array = new String[args.length - 1];
-        for (int i = 1; i <= args.length; i++) {
-            array[i - 1] = args[i];
+        List<FileInputStream> fisList = new ArrayList<>();
+        for (int i = 0; i < filePartCount; i++) {
+            fisList.add(new FileInputStream(fileNamePart[i]));
         }
 
-        Arrays.sort(array);
+        SequenceInputStream sequenceInputStream = new SequenceInputStream(Collections.enumeration
+                (fisList));
+        ZipInputStream zipInputStream = new ZipInputStream(sequenceInputStream);
+        FileOutputStream fileOutputStream = new FileOutputStream(resultFileName);
 
+        byte[] buf = new byte[1024 * 1024];
+        while (zipInputStream.getNextEntry() != null) {
+            int count;
+            while ((count = zipInputStream.read(buf)) != -1) {
+                fileOutputStream.write(buf, 0, count);
+            }
+        }
 
-
+        sequenceInputStream.close();
+        zipInputStream.close();
+        fileOutputStream.close();
     }
 }
