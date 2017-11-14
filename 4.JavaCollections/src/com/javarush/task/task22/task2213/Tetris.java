9a0e215a9ece7fbb52b9e4a0995a1bfba8d01538
Alex Tsova <a.tsova@loopme.com>
2017-11-14 16:26:43 +0200

    big 4 from 2022 task

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -3,7 +3,16 @@ package com.javarush.task.task22.task2213;
 public class Tetris {
     private Field field;
     private Figure figure;
+
     public static void main(String[] args) {
 
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
     }
 }
