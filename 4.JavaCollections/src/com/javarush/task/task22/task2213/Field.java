54948597f96d29806ee46b786432739a31bce117
Alex Tsova <a.tsova@loopme.com>
2017-11-15 11:17:45 +0200

    big 15 from 2022 task

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task22.task2213;
 
+import java.util.ArrayList;
+
 /**
  * Класс Field описывает "поле клеток" игры Тетрис
  */
@@ -103,10 +105,28 @@ public class Field {
      * Удаляем заполненные линии
      */
     public void removeFullLines() {
-        //Например так:
         //Создаем список для хранения линий
+        ArrayList<int[]> lines = new ArrayList<int[]>();
+
         //Копируем все непустые линии в список.
+        for (int i = 0; i < height; i++) {
+            //подсчитываем количество единиц в строке - просто суммируем все ее значения
+            int count = 0;
+            for (int j = 0; j < width; j++) {
+                count += matrix[i][j];
+            }
+
+            //Если сумма строки не равна ее ширине - добавляем в список
+            if (count != width)
+                lines.add(matrix[i]);
+        }
+
         //Добавляем недостающие строки в начало списка.
+        while (lines.size() < height) {
+            lines.add(0, new int[width]);
+        }
+
         //Преобразуем список обратно в матрицу
+        matrix = lines.toArray(new int[height][width]);
     }
 }
