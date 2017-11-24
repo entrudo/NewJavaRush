69be8d59d2ba430019f72f24e83439bf2aff374d
Alex Tsova <a.tsova@loopme.com>
2017-11-24 17:59:45 +0200

    big 13 task #2515

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -1,8 +1,10 @@
 package com.javarush.task.task25.task2515;
 
 public class SpaceShip extends BaseObject {
-    public SpaceShip(double x, double y, double radius) {
-        super(x, y, radius);
+    private double dx = 0;
+
+    public SpaceShip(double x, double y) {
+        super(x, y, 3);
     }
 
     @Override
@@ -12,5 +14,13 @@ public class SpaceShip extends BaseObject {
 
     public void draw() {
 
+    }
+
+    public void moveLeft() {
+        dx = -1;
+    }
+
+    public void moveRight() {
+        dx = 1;
     }
 }
