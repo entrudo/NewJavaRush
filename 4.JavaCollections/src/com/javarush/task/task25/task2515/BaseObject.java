88bba0183e70c1a7df5bdd9270351b3781fd4edd
Alex Tsova <a.tsova@loopme.com>
2017-11-24 10:20:06 +0200

    big 6 task #2515

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
@@ -1,8 +1,47 @@
 package com.javarush.task.task25.task2515;
 
 public abstract class BaseObject {
+    private double x;
+    private double y;
+    private double radius;
+    private boolean isAlive;
+
     public abstract void move();
 
     public abstract void draw();
 
+    public BaseObject(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.isAlive = true;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 }
--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task25.task2515;
 
 public class Bomb extends BaseObject {
+    public Bomb(double x, double y, double radius) {
+        super(x, y, radius);
+    }
+
     @Override
     public void move() {
 
