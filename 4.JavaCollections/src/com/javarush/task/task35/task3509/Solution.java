5ab991da563b64086dc5876d65f546635f018706
Alex Tsova <a.tsova@loopme.com>
2017-11-27 17:02:41 +0200

    finish 33 level with big task and task from 35 level

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3509/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task35.task3509;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+
+/* 
+Collections & Generics
+*/
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+
+    public static ArrayList newArrayList(Object... elements) {
+        //напишите тут ваш код
+        return null;
+    }
+
+    public static HashSet newHashSet(Object... elements) {
+        //напишите тут ваш код
+        return null;
+    }
+
+    public static HashMap newHashMap(List keys, List values) {
+        //напишите тут ваш код
+        return null;
+    }
+}
