519525438ea30972afdb07883514bc72f026c751
Alex Tsova <a.tsova@loopme.com>
2017-12-20 16:30:39 +0200

    finish 34 level and task from 35 level

--- 4.JavaCollections/src/com/javarush/task/task35/task3501/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3501/Solution.java
@@ -1,10 +1,10 @@
-//package com.javarush.task.task35.task3501;
-//
-///*
-//Вызов статического метода
-//*/
-//public class Solution {
-//    public static void main(String[] args) {
-//        Number number = GenericStatic.<Number>someStaticMethod(new Integer(3));
-//    }
-//}
+package com.javarush.task.task35.task3501;
+
+/*
+Вызов статического метода
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Number number = GenericStatic.<Number>someStaticMethod(new Integer(3));
+    }
+}
