a2778286b14842c25b2085f965c83a9791ae2eaa
Alex Tsova <a.tsova@loopme.com>
2017-12-21 22:08:07 +0200

    big task from 37 level 2810 #3

---
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task28.task2810;
+
+import com.javarush.task.task28.task2810.model.Provider;
+
+import java.util.Arrays;
+
+public class Controller {
+    private Provider[] providers;
+
+    public Controller(Provider... providers) {
+        if (providers.length == 0) {
+            throw new IllegalArgumentException();
+        } else {
+            this.providers = providers;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Controller{" +
+                "providers=" + Arrays.toString(providers) +
+                '}';
+    }
+}
