212bb955693e0df7c3a9bd3b2c54e2a590f9d241
Alex Tsova <a.tsova@loopme.com>
2017-12-21 11:07:48 +0200

    big task from 35 level 3702 #3

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
@@ -1,7 +1,11 @@
 package com.javarush.task.task37.task3702;
 
+import com.javarush.task.task37.task3702.male.MaleFactory;
+
 public class Solution {
     public static void main(String[] args) {
-        
+        System.out.println(new MaleFactory().getPerson(99));
+        System.out.println(new MaleFactory().getPerson(4));
+        System.out.println(new MaleFactory().getPerson(15));
     }
 }
---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/male/MaleFactory.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task37.task3702.male;
+
+import com.javarush.task.task37.task3702.Human;
+
+public class MaleFactory {
+    public Human getPerson(int age) {
+        if (age <= 12) {
+            return new KidBoy();
+        } else if (age > 12 && age <= 19) {
+            return new TeenBoy();
+        } else {
+            return new Man();
+        }
+    }
+}
