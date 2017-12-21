4eec5fba8466e3d2dfd44d82122f14c32336d336
Alex Tsova <a.tsova@loopme.com>
2017-12-21 23:42:54 +0200

    big task from 37 level 2810 #14

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -7,8 +7,10 @@ import com.javarush.task.task28.task2810.view.HtmlView;
 
 public class Aggregator {
     public static void main(String[] args) {
+
         Provider provider = new Provider(new HHStrategy());
         HtmlView view = new HtmlView();
+        view.get();
         Model model = new Model(view, provider);
         Controller controller = new Controller(model);
         view.setController(controller);
--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -2,15 +2,26 @@ package com.javarush.task.task28.task2810.view;
 
 import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class HtmlView implements View {
-    Controller controller;
+    private Controller controller;
+    private final String filePath = "./src/" + this.getClass().getPackage().getName().replace('.', '/') + "/vacancies.html";
 
+    public void get() {
+        System.out.println(filePath);
+    }
     @Override
     public void update(List<Vacancy> vacancies) {
+        updateFile(getUpdatedFileContent(vacancies));
+
         System.out.println(vacancies.size());
+
     }
 
     @Override
@@ -18,7 +29,23 @@ public class HtmlView implements View {
         this.controller = controller;
     }
 
+
     public void userCitySelectEmulationMethod() {
         controller.onCitySelect("Odessa");
+    }
+
+
+    private String getUpdatedFileContent(List<Vacancy> list) {
+        return null;
+    }
+
+    private void updateFile(String string) {
+
+
+    }
+
+    protected Document getDocument() throws IOException {
+        return Jsoup.parse(new File(filePath), "UTF-8");
+
     }
 }
