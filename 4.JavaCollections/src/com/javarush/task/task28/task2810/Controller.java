9e0e2f3ffbcd67a8f6dcd7b7bcfdc562a6b82517
Alex Tsova <a.tsova@loopme.com>
2017-12-21 23:23:40 +0200

    big task from 37 level 2810 #12

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
@@ -1,40 +1,18 @@
 package com.javarush.task.task28.task2810;
 
-import com.javarush.task.task28.task2810.model.Provider;
-import com.javarush.task.task28.task2810.vo.Vacancy;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.javarush.task.task28.task2810.model.Model;
 
 public class Controller {
-    private Provider[] providers;
+    private Model model;
 
-    public Controller(Provider... providers) {
-        if (providers.length == 0) {
+    public Controller(Model model) {
+        this.model = model;
+        if (model == null) {
             throw new IllegalArgumentException();
-        } else {
-            this.providers = providers;
         }
     }
 
-    @Override
-    public String toString() {
-        return "Controller{" +
-                "providers=" + Arrays.toString(providers) +
-                '}';
-    }
-
-    public void scan() {
-        List<Vacancy> vacancies = new ArrayList<>();
-        for (Provider provider : providers) {
-            try {
-                for (Vacancy vacancy : provider.getJavaVacancies("Java")) {
-                    vacancies.add(vacancy);
-                }
-            } catch (NullPointerException ignored) {
-            }
-        }
-        System.out.println(vacancies.size());
+    public void onCitySelect(String cityName) {
+        model.selectCity(cityName);
     }
 }
