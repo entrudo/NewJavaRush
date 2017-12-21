822ade1fa602a8598c2cbec846e70a27d3a1719f
Alex Tsova <a.tsova@loopme.com>
2017-12-21 11:21:36 +0200

    add task from 36 level and big task for this level is 3310

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3605/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task36.task3605;
+
+import java.io.IOException;
+
+/* 
+Использование TreeSet
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+    }
+}
