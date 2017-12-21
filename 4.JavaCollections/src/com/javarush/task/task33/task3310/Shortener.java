a00b1115000d8a8d30f960a51665fe3e4532c3a9
Alex Tsova <a.tsova@loopme.com>
2017-12-21 13:44:34 +0200

    big task from 36 level #4

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -1,12 +1,26 @@
 package com.javarush.task.task33.task3310;
 
-public class Shortener {
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
 
-    public Long getId(String string) {
-        return null;
+public class Shortener {
+    private Long lastId = 0L;
+    private StorageStrategy storageStrategy;
+
+    public Shortener(StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
     }
 
-    public String getString(Long id) {
-        return null;
+    public synchronized Long getId(String string) {
+        if (storageStrategy.containsValue(string)) {
+            return storageStrategy.getKey(string);
+        } else {
+            lastId += 1L;
+            storageStrategy.put(lastId, string);
+            return lastId;
+        }
+    }
+
+    public synchronized String getString(Long id) {
+        return storageStrategy.getValue(id);
     }
 }
