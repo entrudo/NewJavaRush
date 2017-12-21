3d3aac5366f517e64e2d25dd1658ce35893bc2cf
Alex Tsova <a.tsova@loopme.com>
2017-12-21 23:44:57 +0200

    big task from 37 level 2810 #16

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -4,8 +4,12 @@ import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
@@ -36,11 +40,47 @@ public class HtmlView implements View {
 
 
     private String getUpdatedFileContent(List<Vacancy> list) {
-        return null;
+        Document document;
+        try {
+            document = getDocument();
+            Elements elementVacancy = document.select("tr[class=\"vacancy\"]");
+            for (Element element : elementVacancy) {
+                element.remove();
+            }
+            Element elementTemplate = document.getElementsByClass("template").first();
+            Element cloneElementTemplate = elementTemplate.clone();
+            cloneElementTemplate.removeClass("template");
+            cloneElementTemplate.removeAttr("style");
+
+
+            for (Vacancy vacancy : list) {
+
+                Element htmlVacancy = cloneElementTemplate.clone();
+                htmlVacancy.select("a[href=\"url\"]").first().text(vacancy.getTitle()).attr("href", vacancy.getUrl());
+                htmlVacancy.select("td[class=\"city\"]").first().text(vacancy.getCity());
+                htmlVacancy.select("td[class=\"companyName\"]").first().text(vacancy.getCompanyName());
+                htmlVacancy.select("td[class=\"salary\"]").first().text(vacancy.getSalary());
+                elementTemplate.before(htmlVacancy.outerHtml());
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Some exception occurred";
+        }
+
+
+        return document.toString();
     }
 
     private void updateFile(String string) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
 
+            bw.write(string);
+
+        } catch (IOException e) {
+            System.out.println("Запись в vacancies не удалась" + e.toString());
+        }
 
     }
 
