70b4a50088cc72c098cfc52f99d2f2a2467e6813
Alex Tsova <a.tsova@loopme.com>
2017-12-21 22:52:54 +0200

    big task from 37 level 2810 #7

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,7 +1,10 @@
 package com.javarush.task.task28.task2810.model;
 
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
 import java.util.List;
 
 public class HHStrategy implements Strategy {
@@ -9,6 +12,12 @@ public class HHStrategy implements Strategy {
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
+        try {
+            Document doc = Jsoup.connect(String.format(URL_FORMAT, "Kiev", 1)).get();
+            String str = doc.html();
+            System.out.println(doc.title());
+        } catch (IOException e) {
+        }
         return null;
     }
 }
