af8874db3841eb8a380b4669fbebe7ff026092cb
Alex Tsova <a.tsova@loopme.com>
2017-12-21 14:31:39 +0200

    finish 36 level and task from 37 level with big task 3913

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3703/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task37.task3703;
+
+/* 
+Найти класс по описанию
+*/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getExpectedClass());
+    }
+
+    public static Class getExpectedClass() {
+        return null;
+    }
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3712/Solution.java
@@ -0,0 +1,20 @@
+//package com.javarush.task.task37.task3712;
+//
+//import java.util.ArrayList;
+//
+///*
+//Шаблонный метод
+//*/
+//public class Solution {
+//    public static void main(String[] args) {
+//        List<Game> games = new ArrayList<>();
+//        games.add(new Football());
+//        games.add(new Basketball());
+//        games.add(new Tennis());
+//
+//        for (Game game : games) {
+//            game.run();
+//            System.out.println("---------------------------------------------");
+//        }
+//    }
+//}
