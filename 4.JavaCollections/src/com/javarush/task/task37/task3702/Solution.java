1828672f77f8c4e387908f8cf8759cf56203ca78
Alex Tsova <a.tsova@loopme.com>
2017-12-21 11:11:14 +0200

    big task from 35 level 3702 #4

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
@@ -1,11 +1,18 @@
 package com.javarush.task.task37.task3702;
 
-import com.javarush.task.task37.task3702.male.MaleFactory;
-
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(new MaleFactory().getPerson(99));
-        System.out.println(new MaleFactory().getPerson(4));
-        System.out.println(new MaleFactory().getPerson(15));
+        AbstractFactory factory = FactoryProducer.getFactory(FactoryProducer.HumanFactoryType.FEMALE);
+        useFactory(factory);
+
+        factory = FactoryProducer.getFactory(FactoryProducer.HumanFactoryType.MALE);
+        useFactory(factory);
     }
+
+    public static void useFactory(AbstractFactory factory) {
+        System.out.println(factory.getPerson(99));
+        System.out.println(factory.getPerson(4));
+        System.out.println(factory.getPerson(15));
+    }
+
 }
---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/female/FemaleFactory.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task37.task3702.female;
+
+import com.javarush.task.task37.task3702.Human;
+
+public class FemaleFactory {
+    public Human getPerson(int age) {
+        if (age <= 12) {
+            return new KidGirl();
+        } else if (age > 12 && age <= 19) {
+            return new TeenGirl();
+        } else {
+            return new Woman();
+        }
+    }
+}
