133d0aec790a856fb86f1d3d63a8f70b455054fb
Alex Tsova <a.tsova@loopme.com>
2017-12-21 23:29:06 +0200

    big task from 37 level 2810 #13

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -1,6 +1,17 @@
 package com.javarush.task.task28.task2810;
 
+import com.javarush.task.task28.task2810.model.HHStrategy;
+import com.javarush.task.task28.task2810.model.Model;
+import com.javarush.task.task28.task2810.model.Provider;
+import com.javarush.task.task28.task2810.view.HtmlView;
+
 public class Aggregator {
     public static void main(String[] args) {
+        Provider provider = new Provider(new HHStrategy());
+        HtmlView view = new HtmlView();
+        Model model = new Model(view, provider);
+        Controller controller = new Controller(model);
+        view.setController(controller);
+        view.userCitySelectEmulationMethod();
     }
 }
